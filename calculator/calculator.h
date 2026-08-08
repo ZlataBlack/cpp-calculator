3fdba72858ca8a932ec5d6ffa15461dfa240e0e1
ZlataBlack <zlata109vika@gmail.com>
2026-08-08 23:12:27 +0300

    Update calculator.h

--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <cmath>
+
 using Number = double;
 
-bool ReadNumber(Number& result);
-bool RunCalculatorCycle();
+class Calculator {
+public:
+    void Set(Number number);
+    Number GetNumber() const;
+
+    void Add(Number addend);
+    void Sub(Number subtrahend);
+    void Mul(Number multiplier);
+    void Div(Number divisor);
+    void Pow(Number exponent);
+
+private:
+    Number number_ = 0;
+};
