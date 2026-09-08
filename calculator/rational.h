db62c133767d0a406f173681331ad205d022d095
ZlataBlack <zlata109vika@gmail.com>
2026-09-08 15:23:42 +0300

    Update rational.h

--- calculator/rational.h
+++ calculator/rational.h
@@ -8,27 +8,27 @@
 
 class Rational {
 public:
-    // Конструктор по умолчанию 0/1
     Rational() : numerator_(0), denominator_(1) {}
 
-    // Конструктор. Числитель и знаменатель = 1
-    Rational(int numerator) : numerator_(numerator), denominator_(1) {}
+    Rational(std::int64_t numerator) : numerator_(numerator), denominator_(1) {}
 
-    // Конструктор из двух чисел
-    Rational(int numerator, int denominator) : numerator_(numerator), denominator_(denominator) {
+    Rational(std::int64_t numerator, std::int64_t denominator) : numerator_(numerator), denominator_(denominator) {
         if (denominator_ == 0) {
             throw std::invalid_argument("Denominator cannot be zero");
         }
         Reduction();
     }
 
-    // Конструктор копирования и оператор присваивания
     Rational(const Rational& other) = default;
     Rational& operator=(const Rational& other) = default;
 
-    // Публичные методы
-    int GetNumerator() const { return numerator_; }
-    int GetDenominator() const { return denominator_; }
+    std::int64_t GetNumerator() const {
+        return numerator_;
+    }
+
+    std::int64_t GetDenominator() const {
+        return denominator_;
+    }
 
     Rational Inv() const {
         if (numerator_ == 0) {
@@ -37,7 +37,6 @@ public:
         return Rational(denominator_, numerator_);
     }
 
-    // Присваивание операций
     Rational& operator+=(const Rational& rhs) {
         numerator_ = numerator_ * rhs.denominator_ + rhs.numerator_ * denominator_;
         denominator_ *= rhs.denominator_;
@@ -69,16 +68,41 @@ public:
         return *this;
     }
 
-    Rational operator+(const Rational& rhs) const { Rational r = *this; r += rhs; return r; }
-    Rational operator-(const Rational& rhs) const { Rational r = *this; r -= rhs; return r; }
-    Rational operator*(const Rational& rhs) const { Rational r = *this; r *= rhs; return r; }
-    Rational operator/(const Rational& rhs) const { Rational r = *this; r /= rhs; return r; }
-    Rational operator+() const { return *this; }
-    Rational operator-() const { return Rational(-numerator_, denominator_); }
+    Rational operator+(const Rational& rhs) const {
+        Rational r = *this;
+        r += rhs;
+        return r;
+    }
+
+    Rational operator-(const Rational& rhs) const {
+        Rational r = *this;
+        r -= rhs;
+        return r;
+    }
+
+    Rational operator*(const Rational& rhs) const {
+        Rational r = *this;
+        r *= rhs;
+        return r;
+    }
+
+    Rational operator/(const Rational& rhs) const {
+        Rational r = *this;
+        r /= rhs;
+        return r;
+    }
+
+    Rational operator+() const {
+        return *this;
+    }
+
+    Rational operator-() const {
+        return Rational(-numerator_, denominator_);
+    }
 
     auto operator<=>(const Rational& rhs) const {
-        int64_t lhs_val = static_cast<int64_t>(numerator_) * rhs.denominator_;
-        int64_t rhs_val = static_cast<int64_t>(rhs.numerator_) * denominator_;
+        std::int64_t lhs_val = numerator_ * rhs.denominator_;
+        std::int64_t rhs_val = rhs.numerator_ * denominator_;
         return lhs_val <=> rhs_val;
     }
 
@@ -93,15 +117,16 @@ public:
     }
 
     friend std::istream& operator>>(std::istream& is, Rational& r) {
-        int num = 0;
+        std::int64_t num = 0;
         is >> num;
-        if (!is) return is;
+        if (!is) {
+            return is;
+        }
 
-        int den = 1;
+        std::int64_t den = 1;
         char slash;
-        is >> std::ws; // Пропускаем пробелы
+        is >> std::ws;
 
-        // Проверяем, есть ли дробная часть
         if (is.peek() == '/') {
             is.get(slash);
             is >> den;
@@ -120,17 +145,16 @@ public:
     }
 
 private:
-    // Метод приведения дроби к несократимому виду
     void Reduction() {
         if (denominator_ < 0) {
             numerator_ = -numerator_;
             denominator_ = -denominator_;
         }
-        const int divisor = std::gcd(numerator_, denominator_);
+        const std::int64_t divisor = std::gcd(numerator_, denominator_);
         numerator_ /= divisor;
         denominator_ /= divisor;
     }
 
-    int numerator_;
-    int denominator_;
+    std::int64_t numerator_;
+    std::int64_t denominator_;
 };
